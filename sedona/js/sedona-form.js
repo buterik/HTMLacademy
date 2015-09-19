3b27aed6299143793a4f07ac1b5c431d983f5bdd
buterik <baranovajane@gmail.com>
2015-09-19 19:29:35 +0100

    sedona index+hotels
    доверстана функциональность, написала JS для формы на главной и кнопок
    +-

--- sedona/js/sedona-form.js
+++ sedona/js/sedona-form.js
@@ -1,6 +1,6 @@
 
-	var legend = document.querySelector(".legend");
-	var animation = document.querySelector(".form-animation");
+	var legend = document.querySelector(".booking-form-show-btn");
+	var animation = document.querySelector(".booking-form-container");
 
 	legend.addEventListener("click", function(event) {
 		event.preventDefault();
