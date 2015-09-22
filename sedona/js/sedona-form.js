c83a980587a7a80b1b6c887ad7c74599bbaddf84
buterik <baranovajane@gmail.com>
2015-09-22 20:45:23 +0100

    финальные правки
    поправлены замечания

--- sedona/js/sedona-form.js
+++ sedona/js/sedona-form.js
@@ -1,8 +1,12 @@
 
-	var legend = document.querySelector(".booking-form-show-btn");
-	var animation = document.querySelector(".booking-form-container");
+var legend = document.querySelector(".booking-form-show-btn");
+var form = document.querySelector(".booking-form-container");
 
-	legend.addEventListener("click", function(event) {
-		event.preventDefault();
-		animation.classList.add("shown-form");
-	});
+legend.addEventListener("click", function(event) {
+	event.preventDefault();
+	form.classList.add("shown-form");
+});
+
+window.addEventListener("load", function() {
+	form.classList.add("hidden-form");
+});
